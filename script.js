b4c851b071a4536d335a0b74ef902b44e02fcebe
krun-krit <thanakit_lue@cmu.ac.th>
2021-06-24 21:17:35 +0700

    update search function

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+var tBody = document.getElementById('tBody')
+
 function showAllAnime(title){
   fetch(`https://api.jikan.moe/v3/search/anime?q=${title}`)
   .then((response) => {
       return response.json()
   }).then ((anime) => {
-      console.log(anime.results)
       addAnimeList(anime.results)
   })
 }
@@ -14,7 +15,6 @@ function onLoad(){
 
 function clicky(){
   let search = document.getElementById('search').value
-  console.log(search)
   showAllAnime(search)
 }
 
@@ -22,27 +22,32 @@ function addAnimeToTable(anime){
   const tBody = document.getElementById('tBody')
   let display = document.createElement('div')
   display.classList.add("card")
+  display.classList.add("col-3")
   let img = document.createElement('img')
   img.setAttribute('src', anime.image_url)
   img.height = 200
   img.width = 150
-  img.addEventListener('click', function(){
-    let confirmMsg = confrim(`Do you want add ${title} to your favorit list? `)
-    if(confirmMsg){
-        console.log(anime.results)
-    }
-  })
+  // img.addEventListener('click', function(){
+  //   let confirmMsg = confrim(`Do you want add ${title} to your favorit list? `)
+  //   if(confirmMsg){
+  //       console.log(anime.results)
+  //   }
+  // })
   display.appendChild(img)
 
   let displayBody = document.createElement('div')
   let title = document.createElement('h5')
   title.innerHTML = anime.title
+  let detail = document.createElement('p')
+  detail.innerHTML = anime.synopsis
   displayBody.appendChild(title)
+  displayBody.appendChild(detail)
   display.appendChild(displayBody)
   tBody.appendChild(display)
 }
 
 function addAnimeList(animeList){
+  tBody.innerHTML = ''
   for (anime of animeList){
       addAnimeToTable(anime) 
   }
