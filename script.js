19f26cf6d7bd988fe3e92af099c4369ab9b8e5ea
krun-krit <thanakit_lue@cmu.ac.th>
2021-06-24 20:17:36 +0700

    test

---
+++ script.js
@@ -0,0 +1,48 @@
+function showAllAnime(title){
+  fetch(`https://api.jikan.moe/v3/search/anime?q=${title}`)
+  .then((response) => {
+      return response.json()
+  }).then ((anime) => {
+      console.log(anime.results)
+      addAnimeList(anime.results)
+  })
+}
+
+function onLoad(){
+  // showAllAnime()
+}
+
+function clicky(){
+  let search = document.getElementById('search').value
+  showAllAnime(search)
+}
+
+function addAnimeToTable(anime){
+  const tBody = document.getElementById('tBody')
+  let display = document.createElement('div')
+  display.classList.add("card")
+  let img = document.createElement('img')
+  img.setAttribute('src', anime.image_url)
+  img.height = 200
+  img.width = 150
+  display.appendChild(img)
+  // img.addEventListener('click', function(){
+  //   let confirmMsg = confrim(`Do you want add ${title} to your favorit list? `)
+  //   if(confirmMsg){
+
+  //   }
+  // })
+
+  let displayBody = document.createElement('div')
+  let title = document.createElement('h5')
+  title.innerHTML = anime.title
+  displayBody.appendChild(title)
+  display.appendChild(displayBody)
+  tBody.appendChild(display)
+}
+
+function addAnimeList(animeList){
+  for (anime of animeList){
+      addAnimeToTable(anime) 
+  }
+}
